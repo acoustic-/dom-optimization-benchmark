f5881daaf14d61be246802f460c808d46459e0bd
Jari Kuisma <jari.kuisma@bitwise.fi>
2017-04-10 19:33:46 +0300

    Adds React implementation.

---
+++ react-impl/src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './App.css';
+import RowItem from './components/RowItem.js';
+
+class App extends Component {
+
+  constructor() {
+        super();
+        this.state = {
+            count: 0
+        };
+    }
+
+  render() {
+    return (
+      <div id="wrap">
+        <div className="header">
+          <h1>Virtual DOM Imp&shy;le&shy;ment&shy;ation</h1>
+          <span className="subheader">~ with React ~</span>
+          <div className="btn-row">
+            <button className="btn btn-primary outline" id="update-all" onClick={() => this.updateAll()}>Update All!</button>
+            <button className="btn btn-primary outline" id="update-part" onClick={() => this.updateSome()}>Update Part!</button>
+            <button className="btn btn-primary outline" id="remove" onClick={() => this.remove()}>Remove!</button>
+          </div>
+          <div className="btn-row">
+            <button className="btn btn-primary outline add-btn" onClick={() => this.add(1000)}>1000</button>
+            <button className="btn btn-primary outline add-btn" onClick={() => this.add(3000)}>3000</button>
+            <button className="btn btn-primary outline add-btn" onClick={() => this.add(6000)}>6000</button>
+            <button className="btn btn-primary outline add-btn" onClick={() => this.add(10000)}>10000</button>
+            <button className="btn btn-primary outline add-btn" onClick={() => this.add(30000)}>30000</button>
+            <button className="btn btn-primary outline add-btn" onClick={() => this.add(60000)}>60000</button>
+            <button className="btn btn-primary outline add-btn" onClick={() => this.add(100000)}>100000</button>
+          </div>
+        </div>
+
+        <div className="container" id="container-for-row-items">
+          { this.renderItems() }
+        </div>
+      </div>
+    );
+  }
+
+  renderItems() {
+    let items = [];
+    var _refi = 0;
+    var makeRef = function () { return 'RowItem-' + (_refi++); };
+    for (var i = 0; i < this.state.count; ++i) {
+      var val = "item-" + i;
+      items.push(<RowItem ref={ makeRef() } key={ val }></RowItem>);
+    }
+    return items;
+  }
+
+  // refsToArray and makeRef functions based on: http://stackoverflow.com/a/27561036
+  refsToArray(ctx, prefix) {
+    let results = [];
+    for (var i = 0; ; i++) {
+      var ref = ctx.refs[prefix + '-' + String(i)];
+      if (ref) results.push(ref);
+      else return results;
+    }
+  }
+
+  add(count) {
+    this.setState({count: count})
+    this.render();
+  }
+
+  updateAll() {
+    this.refsToArray(this, "RowItem").forEach( (i) => i.update() );
+    this.afterOperation();
+  }
+
+  updateSome() {
+    this.refsToArray(this, "RowItem").forEach( (i) => i.updateSome() );
+    this.afterOperation();
+  }
+
+  remove() {
+    this.setState({count: 0})
+    this.afterOperation();
+  }
+
+  afterOperation() {
+    this.render();
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate")
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    window.scrollTo(0, document.body.scrollHeight);
+  }
+
+}
+
+export default App;
---
+++ react-impl/src/components/RowItem.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './RowItem.css';
+
+class RowItem extends Component {
+    constructor() {
+        super();
+        this.state = {
+            content: this.randomContent()
+        };
+        this.style.backgroundColor = this.generateColor();
+        this.size = this.getRandomSize();
+    }
+
+    render() {
+        return (
+            <div className="row-item" style={ this.style }>{ this.state.content }</div>
+        );
+    }
+
+    size = this.getRandomSize();
+
+    style = {
+        backgroundColor: this.generateColor(),
+        height: this.size,
+        width: this.size
+    };
+
+    update() {
+        this.setState({ content: this.randomContent() });
+        var clone = Object.assign({}, this.style);
+        this.style = clone;
+        this.style.backgroundColor = this.generateColor();
+        this.size = this.getRandomSize();
+        this.render();
+    }
+
+    updateSome() {
+        var rand = Math.floor((Math.random() * 5) + 1);
+        if (rand === 5) {
+            this.update();
+        }
+    }
+
+    randomContent() {
+        let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let stringLength = 1;
+        let content = "";
+        for (var i = 0; i < stringLength; ++i) {
+            content += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return content;
+    }
+
+    getRandomSize() {
+        return Math.floor((Math.random() * 6) + 1) + 13 + "px";
+    }
+
+    generateColor() {
+        var color = this.generateRandomColor(256, 256, 256);
+        return color;
+    }
+
+    // http://stackoverflow.com/a/43235
+    // http://stackoverflow.com/a/5624139
+    generateRandomColor(inRed, inGreen, inBlue) {
+        var red = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        var blue = Math.floor(Math.random() * 256);
+
+        // mix the color
+        if (inRed && inGreen && inBlue) {
+            red = Math.floor((red + inRed) / 2);
+            green = Math.floor((green + inGreen) / 2);
+            blue = Math.floor((blue + inBlue) / 2);
+        }
+        return "#" + ((1 << 24) + (red << 16) + (green << 8) + blue).toString(16).slice(1);
+    }
+}
+
+export default RowItem;
